17026ae2773844d82a2f6e88833f8038528a3f99
Nhuuu <nqtrinh87@gmail.com>
2019-01-24 15:49:20 -0800

    turning in hw

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var toDos = ["Buy ice cream", "Eat ice cream", "Go to the gym"]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <MyList theList={toDos} />,
+  document.getElementById('root')
+)
